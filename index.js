06b1ef6da94c3c462b3edcf183f3985026dbcf38
rajatkumarpensia <rajatkumarpensia@gmail.com>
2020-11-13 13:48:29 +0530

    title added

--- index.js
+++ index.js
@@ -3,32 +3,43 @@ showNotes();
 //User added note to local storage
 let addbtn = document.getElementById('addbtn');
 let addtextarea = document.getElementById("addText");
+let addtextareatitle = document.getElementById("addTitle");
 
 
 
 
 addbtn.addEventListener("click", function () {
-    if (addtextarea.value == ""){
+    if (addtextareatitle.value == ""){
+        addtextareatitle.classList.add('border-danger');
+    }
+    else if (addtextarea.value == "") {
+        addtextareatitle.classList.remove('border-danger');
         addtextarea.classList.add('border-danger');
     }
-    else{
-        addtextarea.classList.remove('border-danger');
-    let addtext = document.getElementById("addText");
-    let noteList = localStorage.getItem("notes");
-    // let notesObj;
-    if (noteList == null) {
-        notesObj = [];
-    }
     else {
-        notesObj = JSON.parse(noteList);
-    }
-    notesObj.push(addtext.value);
-    localStorage.setItem("notes", JSON.stringify(notesObj));
-    addtext.value = "";
-    // console.log(notesObj);
+        addtextarea.classList.remove('border-danger');
+        let addtext = document.getElementById("addText");
+        let addTitle = document.getElementById("addTitle");
+        let noteList = localStorage.getItem("notes");
+        // let notesObj;
+        if (noteList == null) {
+            notesObj = [];
+        }
+        else {
+            notesObj = JSON.parse(noteList);
+        }
+        let myObj = {
+            title : addTitle.value,
+            text : addtext.value
+        }
+        notesObj.push(myObj);
+        localStorage.setItem("notes", JSON.stringify(notesObj));
+        addtext.value = "";
+        addTitle.value = "";
+        // console.log(notesObj);
 
-    showNotes();
-}
+        showNotes();
+    }
 })
 
 function showNotes() {
@@ -44,8 +55,8 @@ function showNotes() {
         html += `<div class="col-md-4 notecard">
        <div class="card mb-3">
            <div class="card-body">
-               <h5 class="card-title">Note ${index + 1}</h5>
-               <p class="card-text">${element}</p>
+               <h5 class="card-title">${element.title}</h5>
+               <p class="card-text">${element.text}</p>
                <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete</button>
            </div>
        </div>
@@ -88,10 +99,10 @@ search.addEventListener("input", function () {
     let notecards = document.getElementsByClassName("notecard");
     Array.from(notecards).forEach(function (element) {
         let cardTxt = element.getElementsByTagName("p")[0].innerText;
-        if (cardTxt.includes(inputval)){
+        if (cardTxt.includes(inputval)) {
             element.style.display = "block";
         }
-        else{
+        else {
             element.style.display = "none";
         }
         console.log(cardTxt);
